11dc2bb37388d1e81b64ac1e8ee6433c4bd4dd0a
Jason Cassella <jasoncassella1@gmail.com>
2022-07-04 14:23:15 -0400

    this should  be outside the try block

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const input = document.querySelector('input');
 button.addEventListener('click', runFetch);
 
 async function runFetch() {
+	const searchTerm = input.value;
 	try {
-		const searchTerm = input.value;
 		const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0804rL4eo40Vb9eRvq1EDWJZ3S0sfahL&s=${searchTerm}`,
 			{
 				mode: 'cors',
