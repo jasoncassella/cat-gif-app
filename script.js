3277d5ccc5460915ad973020e993cc54c5111ff0
Jason Cassella <jasoncassella1@gmail.com>
2022-07-04 14:14:16 -0400

    converted to async await syntax

--- script.js
+++ script.js
@@ -4,21 +4,18 @@ const input = document.querySelector('input');
 
 button.addEventListener('click', runFetch);
 
-function runFetch() {
-	const searchTerm = input.value;
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=0804rL4eo40Vb9eRvq1EDWJZ3S0sfahL&s=${searchTerm}`,
-		{
-			mode: 'cors',
-		}
-	)
-		.then(response => {
-			return response.json();
-		})
-		.then(response => {
-			img.src = response.data.images.original.url;
-		})
-		.catch(response => {
-			alert(`there were no gifs of ${searchTerm}`);
-		});
+async function runFetch() {
+	try {
+		const searchTerm = input.value;
+		const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0804rL4eo40Vb9eRvq1EDWJZ3S0sfahL&s=${searchTerm}`,
+			{
+				mode: 'cors',
+			}
+		);
+		const data = await response.json();
+		img.src = data.data.images.original.url;
+	} catch (error) {
+		alert(`there were no gifs of ${searchTerm}`);
+	}
+
 }
